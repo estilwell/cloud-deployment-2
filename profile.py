8ef3e68f82cd0dba4cc8ffbaee44a03186b117e0
estilwell <31806580+estilwell@users.noreply.github.com>
2018-09-13 11:28:43 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,7 +18,7 @@ for i in range(numNodes):
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
     iface = node.addInterface("if" + str(i + 1))
     iface.component_id = "eth1"
-    iface.addAddress(rspec.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
+    iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
     link.addInterface(iface)
     if i == 0:
         node.routable_control_ip = True
