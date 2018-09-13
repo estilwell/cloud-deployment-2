de4a22cfae0e88d19f96d169c09da6838d35e141
estilwell <31806580+estilwell@users.noreply.github.com>
2018-09-13 11:08:13 -0400

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,27 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+numNodes = 4
+
+link = request.LAN("lan")
+
+for i in range(numNodes):
+    node = request.XenVM("node-" + str(i + 1))
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+    iface = node.addinterface("if" + str(i + 1))
+    iface.component_id = "eth1"
+    iface.addAddress(rspec.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
+    link.addInterface(iface)
+    if i < 1:
+        node.routable_control_ip = True
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
